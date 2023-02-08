cdf9836b007fe4310f5f78892ae79ca700e6da39
karim-aboelazm <karimaboelazm28@gmail.com>
2023-02-08 16:46:26 +0200

    new

--- mcv/urls.py
+++ mcv/urls.py
@@ -12,31 +12,11 @@ urlpatterns = [
     path("api/car-detail/<int:pk>", api.CarDetailApi.as_view(), name="api_car_detail"),
     path("api/car-filter/<str:kw>", api.CarFilterApi.as_view(), name="api_car_filter"),
     # cardiagnostic Api url
-    path(
-        "api/cardiagnostic-list/",
-        api.CarDiagnosticListApi.as_view(),
-        name="api_cardiagnostic_list",
-    ),
-    path(
-        "api/cardiagnostic-detail/<int:pk>",
-        api.CarDiagnosticDetailApi.as_view(),
-        name="api_cardiagnostic_detail",
-    ),
-    path(
-        "api/cardiagnostic-filter/<str:kw>",
-        api.CarDiagnosticFilterApi.as_view(),
-        name="api_cardiagnostic_filter",
-    ),
+    path("api/cardiagnostic-list/",api.CarDiagnosticListApi.as_view(),name="api_cardiagnostic_list"),
+    path("api/cardiagnostic-detail/<int:pk>",api.CarDiagnosticDetailApi.as_view(),name="api_cardiagnostic_detail"),
+    path("api/cardiagnostic-filter/<str:kw>",api.CarDiagnosticFilterApi.as_view(),name="api_cardiagnostic_filter"),
     # drivers Api url
     path("api/driver-list/", api.DriverListApi.as_view(), name="api_Drivers_list"),
-    path(
-        "api/driver-detail/<int:pk>",
-        api.DriverDetailApi.as_view(),
-        name="api_Drivers_list",
-    ),
-    path(
-        "api/driver-filter/<str:kw>",
-        api.DriverFilterApi.as_view(),
-        name="api_Drivers_filter",
-    ),
+    path("api/driver-detail/<int:pk>",api.DriverDetailApi.as_view(),name="api_Drivers_list"),
+    path("api/driver-filter/<str:kw>",api.DriverFilterApi.as_view(),name="api_Drivers_filter"),
 ]
