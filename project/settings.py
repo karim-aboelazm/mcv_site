7f5855d84e6adff352bba3d13dd942d2b839409c
karim-aboelazm <20912017100480@fci.zu.edu.eg>
2023-04-18 13:04:55 +0200

    Splash

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     'mcv_app',
-    'bootstrap5'
+    'bootstrap5',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -114,14 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
-MEDIAFILES_DIRS = (os.path.join(BASE_DIR,'media'),)
-
-
+MEDIA_URL = "/media/"
+MEDIAFILES_DIRS = (os.path.join(BASE_DIR, "media"),)
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
