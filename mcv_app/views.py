6e08e43311670eb277256907e9ec9d566bb9d69f
karim-aboelazm <20912017100480@fci.zu.edu.eg>
2023-06-09 20:03:01 +0300

    Login Error Message

--- mcv_app/views.py
+++ mcv_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView,FormView,DetailView
 from django.contrib.auth import authenticate , login ,logout
 from .models import *
 from .forms import *
+from django.contrib import messages
 import requests
 class SplashPageView(TemplateView):
     template_name = 'spalsh_page.html'    
@@ -60,23 +61,26 @@ class McvUserLogin(FormView):
         usr = authenticate(username=user_name,password=pass_word)
         if usr is not None and MCVUser.objects.filter(user=usr).exists():
             login(self.request, usr)
+            return super().form_valid(form)
         else:
-            return render(self.request,self.template_name,{'form':self.form_class})
-        return super().form_valid(form)
-
+            messages.error(self.request,'The admin username or password not correct please try again !')
+            return redirect('/admin-login/')
+    
 class McvDriverLogin(FormView):
     template_name= "mcv_driver_login.html"
     form_class = McvDriverLoginForm
     success_url = '/driver-car-detail/'
-    def form_valid(self,form):
-        user_name = form.cleaned_data.get('username')
-        pass_word = form.cleaned_data['password']
-        usr = authenticate(username=user_name,password=pass_word)
-        if usr is not None and Driver.objects.filter(user=usr).exists():
-            login(self.request, usr)
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None and Driver.objects.filter(user=user).exists():
+            login(self.request, user)
+            return super().form_valid(form)
         else:
-            return render(self.request,self.template_name,{'form':self.form_class})
-        return super().form_valid(form)
+            messages.error(self.request,'The driver username or password not correct please try again !')
+            return redirect('/driver-login/')
 
 class DriverDashBoardPageView(MCVDriverRequiredMixin,TemplateView):
     template_name = 'dashboard.html'
